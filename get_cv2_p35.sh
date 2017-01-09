5ce6683c60a17b2311facc60b6c7d874d26b8b28
左岸清河 <zuoanqh@users.noreply.github.com>
2017-01-09 21:38:55 +0900

    Update get_cv2_p35.sh

--- get_cv2_p35.sh
+++ get_cv2_p35.sh
@@ -9,7 +9,15 @@ cd opencv_contrib
 git checkout 3.1.0
 
 cd ~/opencv
-rm -r build
+
+# must fix this
+wget https://raw.githubusercontent.com/opencv/opencv_3rdparty/81a676001ca8075ada498583e4166079e5744668/ippicv/ippicv_linux_20151201.tgz
+mv ippicv_linux_20151201.tgz 3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e/ippicv_linux_20151201.tgz
+# must fix this too >.<
+wget https://raw.githubusercontent.com/zuoanqh/bootstrapping/master/graphcuts_disabled.cpp
+mv graphcuts_disabled.cpp modules/cudalegacy/src/graphcuts.cpp
+
+sudo rm -r build
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
