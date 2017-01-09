8afcb0c90d9e1009e6a35429378007b445c0170a
左岸清河 <zuoanqh@users.noreply.github.com>
2017-01-09 13:49:43 +0900

    Update get_tf_p35.sh
    fix some bugs

--- get_tf_p35.sh
+++ get_tf_p35.sh
@@ -44,24 +44,45 @@ source $HOME/local/lib/bazel/bin/bazel-complete.bash
 git clone --recurse-submodules https://github.com/tensorflow/tensorflow
 cd tensorflow
 ./configure
-# answer the questions
-# Please specify the location of python. [Default is /home/matsubara/local/bin/python]:
-# Do you wish to build TensorFlow with Google Cloud Platform support? [y/N] N
-# No Google Cloud Platform support will be enabled for TensorFlow
-# Do you wish to build TensorFlow with GPU support? [y/N] y
-# GPU support will be enabled for TensorFlow
-# Please specify which gcc nvcc should use as the host compiler. [Default is /usr/bin/gcc]: 
-# Please specify the Cuda SDK version you want to use, e.g. 7.0. [Leave empty to use system default]: 8.0
-# Please specify the location where CUDA  toolkit is installed. Refer to README.md for more details. [Default is /usr/local/cuda]:
-# Please specify the Cudnn version you want to use. [Leave empty to use system default]:
-# Please specify the location where cuDNN  library is installed. Refer to README.md for more details. [Default is /usr/local/cuda]:
-# Please specify a list of comma-separated Cuda compute capabilities you want to build with.
-# You can find the compute capability of your device at: https://developer.nvidia.com/cuda-gpus.
-# Please note that each additional compute capability significantly increases your build time and binary size.
-# [Default is: "3.5,5.2"]:
-bazel build -c opt --config=cuda --verbose_failures //tensorflow/tools/pip_package:build_pip_package
+# answer the questions: replace "soukun" with your username
+#Please specify the location of python. [Default is /home/soukun/local/bin/python]: /home/soukun/local/bin/python3
+#Do you wish to build TensorFlow with Google Cloud Platform support? [y/N] n
+#No Google Cloud Platform support will be enabled for TensorFlow
+#Do you wish to build TensorFlow with Hadoop File System support? [y/N] n
+#No Hadoop File System support will be enabled for TensorFlow
+#Found possible Python library paths:
+#  /home/soukun/local/lib/python3.5/site-packages
+#Please input the desired Python library path to use.  Default is [/home/soukun/local/lib/python3.5/site-packages]
+#
+#Using python library path: /home/soukun/local/lib/python3.5/site-packages
+#Do you wish to build TensorFlow with OpenCL support? [y/N] n
+#No OpenCL support will be enabled for TensorFlow
+#Do you wish to build TensorFlow with CUDA support? [y/N] y
+#CUDA support will be enabled for TensorFlow
+#Please specify which gcc should be used by nvcc as the host compiler. [Default is /usr/bin/gcc]: 
+#Please specify the CUDA SDK version you want to use, e.g. 7.0. [Leave empty to use system default]: 8.0
+#Please specify the location where CUDA 8.0 toolkit is installed. Refer to README.md for more details. [Default is /usr/local/cuda]: 
+#Please specify the Cudnn version you want to use. [Leave empty to use system default]: 
+#Please specify the location where cuDNN  library is installed. Refer to README.md for more details. [Default is /usr/local/cuda]: 
+#Please specify a list of comma-separated Cuda compute capabilities you want to build with.
+#You can find the compute capability of your device at: https://developer.nvidia.com/cuda-gpus.
+#Please note that each additional compute capability significantly increases your build time and binary size.
+#[Default is: "3.5,5.2"]: 
+
+# now add the missing file from Eigen
+mkdir third_party/eigen3/unsupported/Eigen/CXX11/src/Tensor
+cd third_party/eigen3/unsupported/Eigen/CXX11/src/Tensor
+wget https://bitbucket.org/eigen/eigen/raw/9ba936354ee8b73fb1966dcb2d3506387bb357f1/unsupported/Eigen/CXX11/src/Tensor/TensorContractionThreadPool.h
+cd ~/local/src/tensorflow/
+# replace BUILD file to include the file we just added
+wget https://github.com/zuoanqh/bootstrapping/blob/master/BUILD_fixed
+mv BUILD_fixed 
+
+bazel build -c opt --config=cuda --verbose_failures --copt=-march=native //tensorflow/tools/pip_package:build_pip_package
 rm /tmp/tensorflow_pkg/*.whl
 # remove old ones(if any)
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-pip3 install /tmp/tensorflow_pkg/*.whl
+pip install /tmp/tensorflow_pkg/*.whl
 cd ~
+
+echo "please remember to add the environment variables in this script into ~/.profile"
