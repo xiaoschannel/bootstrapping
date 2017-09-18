c37aab812a6a9e92e076f03173c4606249c6f8ce
zuoanqh <zuoanqh@hotmail.com>
2017-09-18 12:12:05 +0900

    fix export paths

--- initial_setup_must_execute_first.sh
+++ initial_setup_must_execute_first.sh
@@ -6,6 +6,7 @@ mkdir local
 mkdir local/src
 mkdir local/tmp
 mkdir bin
-cat "export PATH=$HOME/local/bin:$PATH" >> .bash_profile
+cat "export PATH=$HOME/local/bin:$HOME/bin:$PATH" >> .bash_profile
+cat "export LD_LIBRARY_PATH=$HOME/local/lib:$HOME/local/lib64:$LD_LIBRARY_PATH" >> .bash_profile
 source ~/.bash_profile
 
