fa5b3ba6e80316b39c4e982bfbded4a6fd620735
zuoanqh <zuoanqh@hotmail.com>
2017-09-20 09:48:46 +0900

    update cv version, remove some of the sudo requirements

--- get_cv2_p35.sh
+++ get_cv2_p35.sh
@@ -1,25 +1,16 @@
-cd ~
+cd ~/local/src/
 git clone https://github.com/opencv/opencv.git
 cd opencv
-git checkout 3.1.0
+git checkout 3.3.0
 
-cd ~
+cd ..
 git clone https://github.com/Itseez/opencv_contrib.git
 cd opencv_contrib
-git checkout 3.1.0
+git checkout 3.3.0
 
-cd ~/opencv
+cd ~/local/src/opencv
 
-# must fix this
-wget https://raw.githubusercontent.com/opencv/opencv_3rdparty/81a676001ca8075ada498583e4166079e5744668/ippicv/ippicv_linux_20151201.tgz
-mkdir 3rdparty/ippicv/downloads
-mkdir 3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e
-mv ippicv_linux_20151201.tgz 3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e/ippicv_linux_20151201.tgz
-# must fix this too >.<
-wget https://raw.githubusercontent.com/zuoanqh/bootstrapping/master/graphcuts_disabled.cpp
-mv graphcuts_disabled.cpp modules/cudalegacy/src/graphcuts.cpp
-
-sudo rm -r build
+rm -r build
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
@@ -28,7 +19,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D ENABLE_FAST_MATH=1 \
     -D CUDA_FAST_MATH=1 \
     -D WITH_CUBLAS=1 \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+    -D OPENCV_EXTRA_MODULES_PATH=~/local/src/opencv_contrib/modules \
     -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-7.5 \
     -D PYTHON_DEFAULT_EXECUTABLE=$HOME/local/bin/python3.5 \
     -D PYTHON_INCLUDE_DIRS=$HOME/local/include/python3.5m \
@@ -37,5 +28,5 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D BUILD_opencv_ts=0 ..
 make -j8
 
-sudo make install
+make install
 sudo ldconfig
