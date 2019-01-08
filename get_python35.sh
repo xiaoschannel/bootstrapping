92b7fca38f94895a5d08f4214ae3ee81612b01a6
Xiao Zeng <zuoanqh@hotmail.com>
2019-01-08 16:38:04 +0900

    update to python 3.5.6

--- get_python35.sh
+++ get_python35.sh
@@ -1,10 +1,10 @@
 cd $HOME/local/src
-# building python 3.5.4
+# building python 3.5.6
 rm ./Python-3.5*
-wget https://www.python.org/ftp/python/3.5.4/Python-3.5.4.tar.xz
-tar Jxvf Python-3.5.4.tar.xz
-rm ./Python-3.5.4.tar.xz
-cd Python-3.5.4
+wget https://www.python.org/ftp/python/3.5.6/Python-3.5.6.tar.xz
+tar Jxvf Python-3.5.6.tar.xz
+rm ./Python-3.5.6.tar.xz
+cd Python-3.5.6
 make clean
 ./configure --prefix=$HOME/local/ --enable-shared LDFLAGS=-Wl,-rpath,$HOME/local/lib
 make -j4
